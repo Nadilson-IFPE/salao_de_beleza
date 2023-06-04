aac6af63701fae2fc28344d42a1f79111ecdae0f
Nadilson <nadilson@protonmail.com>
2023-06-04 03:01:19 -0300

    Bug fix no login (aceitava qualquer valor na senha)

--- api/src/services/UsersService.ts
+++ api/src/services/UsersService.ts
@@ -2,134 +2,153 @@ import { compare, hash } from "bcrypt";
 import { ICreate, IUpdate } from "../interfaces/UsersInterface";
 import { UsersRepository } from "../repositories/UsersRepository";
 import { s3 } from "../config/aws";
-import { v4 as uuid } from 'uuid';
+import { v4 as uuid } from "uuid";
 import { sign, verify } from "jsonwebtoken";
 
 class UsersService {
-    private usersRepository: UsersRepository;
+  private usersRepository: UsersRepository;
 
-    constructor() {
-        this.usersRepository = new UsersRepository();
+  constructor() {
+    this.usersRepository = new UsersRepository();
+  }
 
+  async create({ name, email, password }: ICreate) {
+    const findUser = await this.usersRepository.findUserByEmail(email);
+
+    if (findUser) {
+      throw new Error("User already exists!");
     }
 
-    async create({ name, email, password }: ICreate) {
+    const hashPassword = await hash(password, 10);
 
-        const findUser = await this.usersRepository.findUserByEmail(email);
+    const create = await this.usersRepository.create({
+      name,
+      email,
+      password: hashPassword,
+    });
 
-        if (findUser) {
-            throw new Error('User already exists!');
-        }
+    return create;
+  }
 
-        const hashPassword = await hash(password, 10);
+  async update({
+    name,
+    oldPassword,
+    newPassword,
+    avatar_url,
+    user_id,
+  }: IUpdate) {
+    let password;
+    if (oldPassword && newPassword) {
+      const findUserById = await this.usersRepository.findUserById(user_id);
+      if (!findUserById) {
+        throw new Error("User not found!");
+      }
+      const passwordMatch = await compare(oldPassword, findUserById.password);
+      if (!passwordMatch) {
+        throw new Error("Password invalid!");
+      }
 
-        const create = await this.usersRepository.create({
-            name,
-            email,
-            password: hashPassword,
-        });
-
-        return create;
+      password = await hash(newPassword, 10);
+      await this.usersRepository.updatePassword(newPassword, user_id);
     }
 
-    async update({ name, oldPassword, newPassword, avatar_url, user_id }: IUpdate) {
-        let password;
-        if (oldPassword && newPassword) {
-            const findUserById = await this.usersRepository.findUserById(user_id);
-            if (!findUserById) {
-                throw new Error('User not found!');
-            }
-            const passwordMatch = compare(oldPassword, findUserById.password);
-            if (!passwordMatch) {
-                throw new Error('Password invalid!');
-            }
+    if (avatar_url) {
+      const uploadImage = avatar_url?.buffer;
+      const uploadS3 = await s3
+        .upload({
+          Bucket: "semana-heroi",
+          Key: `${uuid()}-${avatar_url?.originalname}`,
+          // ACL: 'public-read',
+          Body: uploadImage,
+        })
+        .promise();
 
-            password = await hash(newPassword, 10);
-            await this.usersRepository.updatePassword(newPassword, user_id);
-        }
+     // console.log("URL image =>", uploadS3.Location);
 
-        if (avatar_url) {
-            const uploadImage = avatar_url?.buffer;
-            const uploadS3 = await s3.upload({
-                Bucket: 'semana-heroi',
-                Key: `${uuid()}-${avatar_url?.originalname}`,
-                // ACL: 'public-read',
-                Body: uploadImage,
-            }).promise();
+      await this.usersRepository.update(name, uploadS3.Location, user_id);
+    }
+    return {
+      message: "User updated successfully!",
+    };
+  }
 
-            console.log('URL image =>', uploadS3.Location);
+  async auth(email: string, password: string) {
+    const findUser = await this.usersRepository.findUserByEmail(email);
 
-            await this.usersRepository.update(name, uploadS3.Location, user_id);
-        }
-        return {
-            message: 'User updated successfully!',
-        };
+    if (!findUser) {
+      throw new Error("User or password invalid!");
     }
 
-    async auth(email: string, password: string) {
-        const findUser = await this.usersRepository.findUserByEmail(email);
+    const passwordMatch = await compare(password, findUser.password);
 
-        if (!findUser) {
-            throw new Error('User or password invalid!');
-        }
-
-        const passwordMatch = compare(password, findUser.password);
-
-        if (!passwordMatch) {
-            throw new Error('User or password invalid!');
-        }
-
-        let secretKey: string | undefined = process.env.ACCESS_WEB_TOKEN_KEY;
-
-        if (!secretKey) {
-            throw new Error('There is no token key!');
-        }
-
-        const token = sign({ email }, secretKey, {
-            subject: findUser.id,
-            expiresIn: 60 * 15, // Poderia ser 15m
-        });
-
-        const refreshToken = sign({ email }, secretKey, {
-            subject: findUser.id,
-            expiresIn: '7d', // 7 dias
-        });
-
-        return {
-            token,
-            refresh_token: refreshToken,
-            user: {
-                name: findUser.name,
-                email: findUser.email,
-            },
-        }
+    if (!passwordMatch) {
+      throw new Error("User or password invalid!");
     }
 
-
-    async refresh(refresh_token: string) {
-
-        if (!refresh_token) {
-            throw new Error('Refresh token missing!');
-        }
-
-        let secretKey: string | undefined = process.env.ACCESS_WEB_TOKEN_KEY;
-
-        if (!secretKey) {
-            throw new Error('There is no refresh token key!');
-        }
-
-        const verifyRefreshToken = verify(refresh_token, secretKey);
-
-        const { sub } = verifyRefreshToken;
-
-
-        const newToken = sign({ sub }, secretKey, {
-            expiresIn: 60 * 15, // Poderia ser 15m
-        });
-
-        return { token: newToken };
+    let secretKey: string | undefined = process.env.ACCESS_WEB_TOKEN_KEY;
+    if (!secretKey) {
+      throw new Error("There is no token key!");
     }
 
+    let secretKeyRefreshToken: string | undefined =
+      process.env.ACCESS_WEB_REFRESH_TOKEN_KEY;
+    if (!secretKeyRefreshToken) {
+      throw new Error("There is no token key!");
+    }
+
+    const token = sign({ email }, secretKey, {
+      subject: findUser.id,
+      //  expiresIn: 60 * 15, // Poderia ser 15m
+      expiresIn: "1h",
+    });
+
+    const refreshToken = sign({ email }, secretKeyRefreshToken, {
+      subject: findUser.id,
+      expiresIn: "7d", // 7 dias
+    });
+
+    return {
+      token,
+      refresh_token: refreshToken,
+      user: {
+        name: findUser.name,
+        email: findUser.email,
+      },
+    };
+  }
+
+  async refresh(refresh_token: string) {
+    if (!refresh_token) {
+      throw new Error("Refresh token missing!");
+    }
+
+    let secretKeyRefreshToken: string | undefined =
+      process.env.ACCESS_WEB_REFRESH_TOKEN_KEY;
+    if (!secretKeyRefreshToken) {
+      throw new Error("There is no refresh token key!");
+    }
+
+    let secretKey: string | undefined =
+      process.env.ACCESS_WEB_TOKEN_KEY;
+    if (!secretKey) {
+      throw new Error("There is no refresh token key!");
+    }
+
+    const verifyRefreshToken = await verify(refresh_token, secretKeyRefreshToken);
+
+    const { sub } = verifyRefreshToken;
+
+    const newToken = sign({ sub }, secretKey, {
+      // expiresIn: 60 * 15, // Poderia ser 15m
+      expiresIn: "1h",
+    });
+
+    const refreshToken = sign({ sub }, secretKeyRefreshToken, {
+      expiresIn: "7d",
+    });
+
+    return { token: newToken, refresh_token: refreshToken };
+  }
 }
 
 export { UsersService };
